8a6e621490c7562c3c5a4f0b728248b3ae12348e
cjyyoyo <592116849@qq.com>
2020-08-13 21:53:22 +0800

    week03更新

--- src/week03/LoopQuene.java
+++ src/week03/LoopQuene.java
@@ -7,7 +7,7 @@ public class LoopQuene<E> implements Quene<E>{
     private int size;
 
     public LoopQuene(int capacity){
-        data=(E[])new Object(capacity+1);
+        data=(E[])new Object[capacity+1];
         front=0;
         tail=0;
         size=0;
@@ -27,7 +27,68 @@ public class LoopQuene<E> implements Quene<E>{
     }
 
     @Override
+    public void enquene(E e) {
+
+        if ((tail+1)%data.length==front)
+            resize(getCapacity()*2);
+
+        data[tail]=e;
+        tail=(tail+1)%data.length;
+        size++;
+    }
+
+    @Override
+    public E dequene() {
+
+        if (isEmpty())
+            throw new IllegalArgumentException("Cannot dequene from an empty quene.");
+
+        E ret=data[front];
+        data[front]=null;
+        front=(front+1)%data.length;
+        size--;
+        if (size==getCapacity()/4&&getCapacity()/2!=0)
+            resize(getCapacity()/2);
+        return ret;
+    }
+
+    private void resize(int newCapacity){
+
+        E[] newData=(E[])new Object[newCapacity+1];
+        for (int i=0;i<size;i++)
+            newData[i]=data[(i+front)%data.length];
+
+        data=newData;
+        front=0;
+        tail=size;
+
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty())
+            throw new IllegalArgumentException("Quene is empty");
+        return data[front];
+
+    }
+
+    @Override
     public int getSize() {
         return size;
+    }
+
+    @Override
+    public String toString(){
+
+        StringBuffer res=new StringBuffer();
+        res.append(String.format("Quene :size = %d , capacity = %d \n",size,data.length));
+        res.append("front [");
+        for (int i =front;i!=tail;i=(i+1)%data.length){
+            res.append(data[i]);
+            if ((i+1)%data.length!=tail)
+                res.append(", ");
+        }
+        res.append("] tail");
+        return res.toString();
     }
 }
--- src/week03/Main.java
+++ src/week03/Main.java
@@ -1,6 +1,25 @@
 package week03;
 
+import java.util.Random;
+
 public class Main {
+
+    private static double testQuene(Quene<Integer> q,int opCount){
+
+        long startTime=System.nanoTime();
+
+        Random random=new Random();
+        for (int i=0;i<opCount;i++)
+            q.enquene(random.nextInt(Integer.MAX_VALUE));
+
+        for (int i=0;i<opCount;i++)
+            q.dequene();
+
+        long endTime=System.nanoTime();
+
+        return (endTime-startTime)/1000000000.0;
+    }
+
     public static void main(String[] args) {
         //使用泛型，因此增加<Integer>,泛型中不能存放基本数据类型
 //        Array<Integer> arr=new Array<>(20);
@@ -22,14 +41,26 @@ public class Main {
 //        System.out.println(arr);
 
 
-        ArrayStack<Integer> stack=new ArrayStack<>();
+//        ArrayStack<Integer> stack=new ArrayStack<>();
+//
+//        for (int i =0;i<5;i++){
+//            stack.push(i);
+//            System.out.println(stack);
+//        }
+//
+//        stack.pop();
+//        System.out.println(stack);
 
-        for (int i =0;i<5;i++){
-            stack.push(i);
-            System.out.println(stack);
-        }
-        
-        stack.pop();
-        System.out.println(stack);
+
+        //测试两种队列的性能
+        int opCount=100000;
+
+        ArrayQuene<Integer> arrayQuene=new ArrayQuene<>();
+        double time1=testQuene(arrayQuene,opCount);
+        System.out.println(" ArrayQuene , time: "+time1+" s");
+
+        LoopQuene<Integer> loopQuene=new LoopQuene<>();
+        double time2=testQuene(loopQuene,opCount);
+        System.out.println(" LoopQuene , time: "+time2+" s");
      }
 }
